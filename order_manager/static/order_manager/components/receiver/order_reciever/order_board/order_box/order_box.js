6bf923225c9ae1b55af80b13631de43c19dafd1c
Seyhak Ly <seyhak.ly@gmail.com>
2020-01-25 13:08:33 +0100

    Set created for products
    TODO add dictionary

--- order_manager/static/order_manager/components/receiver/order_reciever/order_board/order_box/order_box.js
+++ order_manager/static/order_manager/components/receiver/order_reciever/order_board/order_box/order_box.js
@@ -7,9 +7,26 @@ function OrderFieldTitle(props){
     );
   }
 
+function CountProducts(products){
+  const content_set = CreateProductsSet(products);
+  //TODO create dictionary product name: counted
+  for (let i = 0; i < products.length; i++) {
+    
+  }
+}
+
+function CreateProductsSet(products){
+  var products_list = [];
+  for (let i = 0; i < products.length; i++) {
+    products_list.push(products[i].name);
+  }
+  return new Set(products_list);
+}
 
 function OrderFieldContent(props){
   var content = [];
+  var counted_products = CountProducts(props.products);
+  console.log(content_set);
   for(var i = 0; i < props.products.length; i++){
     if(i==0){
       content.push(<li> {props.products[i].name}</li>);
@@ -26,12 +43,14 @@ function OrderFieldContent(props){
     </div>
   );
 }
+
+
 class OrderBox extends React.Component{
 
   render()
   {
-    console.log(1111);
-    console.log(this.props.order);
+    // console.log(1111);
+    // console.log(this.props.order);
     return(
       <button className="order_box btn btn-outline-dark m-1" id={this.props.order.id} onClick={this.props.onOrderClick}>
         <OrderFieldTitle id={this.props.order.id} time={this.props.order.order_date}/>
