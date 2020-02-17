0a1363eb6b92e3afd86ee272ac1e13cba4f03698
Seyhak Ly <seyhak.ly@gmail.com>
2020-02-18 00:08:48 +0100

    Flake job, filter todo

--- BasicBusinessManager/models/order_related_objects/company.py
+++ BasicBusinessManager/models/order_related_objects/company.py
@@ -1,17 +1,20 @@
 from django.db import models
 from BasicBusinessManager.models.users.company_owner import CompanyOwner
 
-# Create your models here.
+
 class Company(models.Model):
     name = models.CharField(max_length=30)
     businesses_owner = models.ManyToManyField("CompanyOwner")
-    address = models.CharField(max_length=60,blank=True)
+    address = models.CharField(max_length=60, blank=True)
     sector = models.ManyToManyField("Sector")
+
     def __str__(self):
         return self.name
 
+
 class Sector(models.Model):
     name = models.CharField(max_length=30)
-    decription = models.CharField(max_length=300,blank=True)
+    decription = models.CharField(max_length=300, blank=True)
+
     def __str__(self):
         return self.name
--- BasicBusinessManager/models/order_related_objects/product.py
+++ BasicBusinessManager/models/order_related_objects/product.py
@@ -1,13 +1,14 @@
+# import datetime
 from django.db import models
-from django.utils import timezone
-import datetime
 from .sale_out import Sale_out
 from .company import Company
 # Create your models here.
+
+
 class Product(models.Model):
     name = models.CharField(max_length=30)
-    price = models.DecimalField(max_digits=10,decimal_places=2)
-    #sale_out = models.ForeignKey(Sale_out,on_delete=models.DO_NOTHING)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    # sale_out = models.ForeignKey(Sale_out,on_delete=models.DO_NOTHING)
     sallers = models.ManyToManyField("Company")
     visible = models.BooleanField(default=True)
 
