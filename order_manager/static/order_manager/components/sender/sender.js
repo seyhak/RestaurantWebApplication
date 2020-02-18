ef435706277e2f4bdf75829314f3b5b204933472
Seyhak Ly <seyhak.ly@gmail.com>
2020-02-18 23:51:08 +0100

    Filter done, TODO Sender

--- order_manager/static/order_manager/components/sender/sender.js
+++ order_manager/static/order_manager/components/sender/sender.js
@@ -18,14 +18,22 @@ class Sender{
       this.products = "";
       this.base_url = window.location.origin;
       var me = this;
+      // TODO company choosing
+      let id_list = [1]
       $.when(this.getProducts()).done((data) => {
         me.products = data;
         me.runSenderUI();
       });
     }
-
-    getProducts(){
-      var url = window.location.origin + "/";
+    //id filtering todo
+    getProducts(id_list){
+      const seller_prefix = "seller="
+      let sellers = []
+      id_list.forEach((item) => {
+        sellers.push(item)
+      })
+      sellers.join("&")
+      let url = window.location.origin + "/products/?" + sellers + "/?visible=true"
       return $.getJSON(url,function(data){
       });
     }
