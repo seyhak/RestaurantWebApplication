642139e2791c7bb0429dc2f646d4e141445367fb
Seyhak Ly <seyhak.ly@gmail.com>
2020-02-21 23:45:26 +0100

    Code reshaping - Fixing orderboard (setInterval)

--- order_manager/static/order_manager/components/receiver/order_reciever/order_receiver.js
+++ order_manager/static/order_manager/components/receiver/order_reciever/order_receiver.js
@@ -1,78 +1,114 @@
 import Cookies from 'js-cookie'
 import OrdersBoard from './order_board/order_board'
 import './order_receiver.css'
+// import {getCookie, csrfSafeMethod} from '../../scripts/csrf';
+
+
+// $.ajaxSetup({
+//   beforeSend: function(xhr, settings) 
+//   {     
+//       var csrftoken = Cookies.get('csrftoken');
+//       if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+//           xhr.setRequestHeader("X-CSRFToken", csrftoken);
+//       }
+//   }
+// });
 
 class OrdersReceiver extends React.Component{//waiting for extensions(? no idea what kind of yet)
   constructor(props){
     super(props);
-    let orders = this.props.orders.results
+    this.workplace = this.props.workplace
     this.state = {
-      currentOrders:orders,
+      orders: []
     }
   }
 
+  componentDidMount() {
+    console.log("Update: " + Date.now())
+    this.interval = setInterval(() => {
+      $.when(this.getUndoneOrders()).done((data) => {
+        this.setState(
+          { orders: data }
+        )
+      });
+    }, 5000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  getUndoneOrdersJsonUrl(){
+    return window.location.origin + "/rest/order/?delivered=false&deliverant=" + this.workplace;
+  }
+  
+  getUndoneOrders(){
+    let url = this.getUndoneOrdersJsonUrl();
+    return $.getJSON(url,function(data){
+    });
+  }
+
   closeOrder(closingOrder){
-    let csrftoken = Cookies.get('csrftoken');
-    let id = closingOrder.id;
-    // console.log(id.toString())
-    // console.log(closing_order)
-    let url = "http://127.0.0.1:8000/rest/order/" + id.toString() + "/";
+    const csrftoken = Cookies.get('csrftoken');
+    const id = closingOrder.id;
+    console.log(id.toString())
+    console.log(closingOrder)
+    const url = window.location.origin + '/rest/order/O/' + id.toString() + "/";
     $.getJSON(url, function(data)
     {
-      let jsonFile = JSON.stringify(data);
-      jsonFile = JSON.parse(jsonFile);
+      const headers = new Headers();
+      const csrftoken = Cookies.get('csrf_token');
+      headers.append('X-CSRFToken', csrftoken);
+      headers.append('Content-Type', 'application/json');
       jsonFile.delivered = true;
       jsonFile = JSON.stringify(jsonFile);
-      // console.log(id)
       $.ajax({
+        headers: headers,
         type: "PUT",
         url: url,
         CSRF: csrftoken,
         data: jsonFile,
-        contentType: "application/json",
         success: () => {
           console.log("order closed " + id.toString());
         },
         error: () => {
           console.log("failed to close the order " + id.toString() + " failed");
         }
-      });
-    });
+      })
+    })
   }
 
   onOrderBoxClick(orderToClose){
-    let tempArr =  this.state.currentOrders;
+    let tempArr =  this.state.orders;
     console.log(orderToClose.id)
     //TODO delete array obj where ID...
     console.log(tempArr)
-    console.log(
-      tempArr.splice(orderToClose.id, 1))
+    console.log(tempArr.splice(orderToClose.id, 1))
     tempArr.splice(orderToClose.id, 1);
     console.log(tempArr)
     this.closeOrder(orderToClose);
     this.setState({
-      currentOrders: tempArr,
+      orders: tempArr,
       });
   }
 
-  renderBoard(){
-    return(
-      <div className="order_board_container">
-      <OrdersBoard 
-        orders={this.state.currentOrders}
-        onOrderClick={id=>this.onOrderBoxClick(id)}
-      />
-      </div>
-    );
-  }
-
+  // renderBoard(){
+  //   return(
+  //     <div className="order_board_container">
+  //     <OrdersBoard 
+  //       orders={this.state.currentOrders}
+  //       onOrderClick={id=>this.onOrderBoxClick(id)}
+  //     />
+  //     </div>
+  //   );
+  // }
 
   render()
   {
     return(
       <div className="order_board_container float-left">
         <OrdersBoard 
-          orders={this.state.currentOrders}
+          orders={this.state.orders}
           onOrderClick={id=>this.onOrderBoxClick(id)}
         />
       </div>
